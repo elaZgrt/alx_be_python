98b27c09c14f47426770a72757e7f5f50d4ecdae
elaZgrt <eliassolomon2013@gmail.com>
2025-05-21 20:17:11 +0300

    Initial commit

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=15
+sum = number1 + number2
+print("addition of 10 and 15 is", sum)
+difference = number2 - number1
+print("subtraction of 10 from 15 is", difference)
+multiplication = number1 * number2
+print("multiplication of 10 and 15 is", multiplication)
