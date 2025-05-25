68846373fe69c950be0c79b81d5a329afc3886b3
elaZgrt <eliassolomon2013@gmail.com>
2025-05-25 20:57:21 +0300

    introduction to python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=5
+sum = number1 + number2
+print("addition of 10 and 15 is", sum)
+difference = number2 - number1
+print("subtraction of 10 and 15 is", difference)
+multiplication = number1 * number2
+print("multiplication of 10 and 15 is", multiplication)
