6e0920855dd540443d43e7a69fb11612b36ccdb3
elaZgrt <eliassolomon2013@gmail.com>
2025-05-26 22:35:47 +0300

    Add 5 control-flow Python scripts

--- python_introduction/basic_operations.py
+++
@@ -1,8 +0,0 @@
-number1 = 10
-number2 = 5
-sum = number1 + number2
-print("addition of 10 and 15 is", sum)
-difference = number2 - number1
-print("subtraction of 10 and 15 is", difference)
-multiplication = number1 * number2
-print("multiplication of 10 and 15 is", multiplication)
--- python_introduction/future_age_calculator.py
+++
@@ -1,3 +0,0 @@
-age = int(input("How old are you?"))
-age = age + 27
-print("In 2050, you will be", age ,"years old.")
